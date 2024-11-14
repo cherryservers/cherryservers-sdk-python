a8535042476b1f0c5565208bd0eb847ac47626c8
Martynas Deveikis <martynasdev@gmail.com>
2024-11-14 13:52:21 +0200

    feat: add base classes

---
+++ cherry/_base.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+
+import abc
+import copy
+from typing import TYPE_CHECKING
+
+from pydantic import BaseModel, ConfigDict
+
+from cherry import _models
+
+if TYPE_CHECKING:
+    from cherry import _client
+
+
+class ResourceModel(BaseModel, abc.ABC):
+    model_config = ConfigDict(frozen=True)
+
+
+class ResourceClient(abc.ABC):  # noqa: B024
+    """Cherry Servers resource client base.."""
+
+    def __init__(self, api_client: _client.CherryApiClient) -> None:
+        """Initialize a Cherry Servers resource client."""
+        self._api_client = api_client
+
+
+class Resource[C: ResourceClient, T: ResourceModel](abc.ABC):  # noqa: B024
+    def __init__(self, client: C, model: T) -> None:
+        """Initialize a Cherry Servers resource."""
+        self._model = model
+        self._client = client
+
+    def get_model_copy(self) -> T:
+        """Get resource model copy.
+
+        Returns a deep copy of the resource model.
+        Use this if you need read access to resource model fields.
+        """
+        return copy.deepcopy(self._model)
+
+
+class RequestSchema(_models.CherryRequestSchema, BaseModel, abc.ABC):
+    """Cherry Servers base API request schema."""
