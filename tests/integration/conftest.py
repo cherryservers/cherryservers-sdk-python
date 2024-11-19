d0ea7249ec4af08882c861b49ca6382257f20ba2
Martynas Deveikis <martynasdev@gmail.com>
2024-11-19 14:55:55 +0200

    test: add package scope vps and project fixtures

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -3,10 +3,15 @@
 from __future__ import annotations
 
 import os
+import time
+from typing import TYPE_CHECKING
 
 import pytest
 
 import cherry
+
+if TYPE_CHECKING:
+    from collections.abc import Generator
 
 
 @pytest.fixture(scope="package")
@@ -23,3 +28,33 @@ def team_id() -> int:
     team_id = os.environ.get("CHERRY_TEST_TEAM_ID")
     assert team_id, "CHERRY_TEST_TEAM_ID environment variable is not set"
     return int(team_id)
+
+
+@pytest.fixture(scope="package")
+def project(
+    team_id: int, facade: cherry.facade.CherryApiFacade
+) -> Generator[cherry.projects.Project]:
+    """Initialize a Cherry Servers project."""
+    creation_req = cherry.projects.CreationRequest(
+        name="cherry-python-sdk-project-fixture"
+    )
+    project = facade.projects.create(creation_req, team_id=team_id)
+    yield project
+    project.delete()
+
+
+@pytest.fixture(scope="package")
+def vps(
+    facade: cherry.facade.CherryApiFacade, project: cherry.projects.Project
+) -> Generator[cherry.servers.Server]:
+    """Initialize a Cherry Servers VPS."""
+    creation_req = cherry.servers.CreationRequest(
+        region="eu_nord_1", plan="cloud_vps_1"
+    )
+
+    vps = facade.servers.create(creation_req, project.get_model_copy().id)
+    while vps.get_model_copy().state != "active":
+        time.sleep(10)
+        vps = facade.servers.get_by_id(vps.get_model_copy().id)
+    yield vps
+    vps.delete()
