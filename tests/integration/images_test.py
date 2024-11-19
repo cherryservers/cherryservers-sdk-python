8ef01480c1885675fbacb41616848cf315fd4b1c
Martynas Deveikis <martynasdev@gmail.com>
2024-11-19 15:37:40 +0200

    test: add images integration tests

---
+++ tests/integration/images_test.py
@@ -0,0 +1,23 @@
+"""Test cherry images module functionality."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    import cherry
+
+
+class TestImage:
+    """Test User functionality."""
+
+    def test_get_by_plan(self, facade: cherry.facade.CherryApiFacade) -> None:
+        """Test getting a list of images by plan slug."""
+        images = facade.images.get_by_plan("cloud_vps_1")
+
+        retrieved_image_models = [image.get_model_copy() for image in images]
+
+        assert any(
+            image_model.slug == "debian_12_64bit"
+            for image_model in retrieved_image_models
+        )
