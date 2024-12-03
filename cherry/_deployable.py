abefa38a52480ee70b264116ada191cd94ba489c
Martynas Deveikis <martynasdev@gmail.com>
2024-12-03 16:53:33 +0200

    feat: add capability to check if resource is deployed

---
+++ cherry/_deployable.py
@@ -0,0 +1,63 @@
+from __future__ import annotations
+
+import abc
+import time
+from random import uniform
+from typing import Protocol
+
+
+class DeploymentTimeoutError(Exception):
+    """Deployment timeout occurred."""
+
+    def __init__(self, msg: str) -> None:
+        super().__init__(f"{msg}")
+
+
+class ResourceModelWithStatus(Protocol):
+    """Resource model than contains a status field."""
+
+    status: str | None
+
+
+class DeployableResource(abc.ABC):
+    """A resource that is deployable."""
+
+    @abc.abstractmethod
+    def get_model_copy(self) -> ResourceModelWithStatus: ...
+
+    @abc.abstractmethod
+    def refresh(self) -> None: ...
+
+
+def wait_for_deployment(
+    resource: DeployableResource,
+    timeout: float,
+) -> None:
+    """Wait for a resource to be deployed.
+
+    :param DeployableResource resource: Resource to wait for.
+    :param float timeout: Timeout in seconds.
+
+    :raises DeploymentTimeoutError: If timeout occurs.
+    """
+    model_copy = resource.get_model_copy()
+    retries = 0
+    while model_copy.status != "deployed":
+        delay = _get_exponential_delay(retries)
+        if delay > timeout:
+            msg = f"timeout waiting for {resource.__class__.__name__} to deploy"
+            raise DeploymentTimeoutError(msg)
+        time.sleep(delay)
+        resource.refresh()
+        model_copy = resource.get_model_copy()
+        retries += 1
+
+
+def _get_exponential_delay(retries: int) -> float:
+    """Get exponential delay in seconds.
+
+    :param int retries: The number of retries that have occurred so far.
+    """
+    max_delay: float = 20
+    delay: float = (2 * 2**retries / 2) + uniform(0, (2 * 2**retries / 2))  # noqa: S311
+    return min(delay, max_delay)
